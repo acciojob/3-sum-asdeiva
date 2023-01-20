bc523f81ab39873867bb6c7f5e66d914d9c556b7
DEIVA SIVAM <115927155+asdeiva@users.noreply.github.com>
2023-01-20 12:05:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function threeSum(arr, target) {
   //your code here
+	for (t in arr){
+		let sum=0;
+		let ans=0;
+		sum+=arr[t];
+			if(target+1<=sum){
+				ans=sum;
+			}
+	}
+	return ans;
 }
 
 module.exports = threeSum;
